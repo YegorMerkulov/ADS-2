cabe3e20dd3b99626b9cde018d576a835474285e
YegorMerkulov <116704193+YegorMerkulov@users.noreply.github.com>
2023-04-07 18:40:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,65 +4,44 @@
 #include "alg.h"
 
 
+
+
 double pown(double value, uint16_t n) {
-    double result = pow(value, n);
-    return result;
+  return pow(value, n);
 }
 
 uint64_t fact(uint16_t n) {
-    if (n == 0) {
-      return 1;
-    } else {
-      return n * fact(n - 1);
+  uint64_t fac = 1;
+    for (int i = 1; i <= n; i++) {
+        fac *=i;
     }
+    return fac;
 }
 
 double calcItem(double x, uint16_t n) {
-  int count = 0;
-  while (x != 0) {
-    x = x-n;
-    count+=1;
-  }
-  return count;
+  return (pow(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-    double result = 1.0;
-    for (int i = 1; i <= count; i++) {
-        result+=pow(x, i)/i;
-    }
-    return result;
+  double exp = 1.0;
+  for (int i = 1; i <= count; i++) {
+    exp += calcItem(x, i);
+  }
+  return exp;
 }
 
 double sinn(double x, uint16_t count) {
-  int n = 0;
-  int g = 1;
-  uint64_t f = 1;
-  int m = 1;
-  double d = x;
-  double sin = 0;
-  for (int i = 0; i < n; i++) {
-    sin = sin+g*d/f;
-    d = d*x*x;
-    f = f*(m+1)*(m+2);
-    m = m+2;
-    g = -g;
+  double sin = 0.0;
+  for (int i = 0; i < count; i++) {
+    sin += calcItem(x, 2*i+1) * pow(-1, i);
   }
-    return sin;
+  return s;
 }
 
 double cosn(double x, uint16_t count) {
-  int g = 1;
-  uint64_t f = 1;
-  int m = 0;
-  double d = x;
-  double cosn = 0;
+  double cos = 0.0;
   for (int i = 0; i < count; i++) {
-    cos = 1-cos*g*d/f;
-    d = d*pow(x, 2);
-    f = f*(m+1)*(m+2);
-    m+=2;
-    g = -g;
+    cos += calcItem(x, 2*i) * pow(-1, i);
   }
-    return cos;
+  return cos;
 }
